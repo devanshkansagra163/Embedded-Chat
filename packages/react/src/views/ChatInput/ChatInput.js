545276363b8232dcc558e093d5dc5d074d3081cf
devanshkansagra163 <devanshkansagra163@gmail.com>
2024-11-07 22:20:26 +0530

    Update ChatInput.js 2

--- packages/react/src/views/ChatInput/ChatInput.js
+++ packages/react/src/views/ChatInput/ChatInput.js
@@ -486,7 +486,7 @@ const ChatInput = ({ scrollToBottom }) => {
             disabled={!isUserAuthenticated || !canSendMsg || isRecordingMessage}
             placeholder={
               isUserAuthenticated && canSendMsg
-                ? 'Message Here'
+                ? 'Message Here All'
                 : isUserAuthenticated
                 ? 'This room is read only'
                 : 'Sign in to chat'
