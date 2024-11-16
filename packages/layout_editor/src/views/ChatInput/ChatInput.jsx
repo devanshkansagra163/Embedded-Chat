d4e35bc9ea1b4c71201633621b2e4515b31bbca5
devanshkansagra163 <devanshkansagra163@gmail.com>
2024-11-16 13:45:56 +0530

    Update ChatInput.jsx

--- packages/layout_editor/src/views/ChatInput/ChatInput.jsx
+++ packages/layout_editor/src/views/ChatInput/ChatInput.jsx
@@ -34,7 +34,7 @@ const ChatInput = () => {
           <Input
             textArea
             rows={1}
-            placeholder="Message"
+            placeholder="Message here all"
             css={styles.textInput}
             onBlur={handleBlur}
             onFocus={handleFocus}
